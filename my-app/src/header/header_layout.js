177cdb57b4371cf342a1ff933644c2d364aa381a
Cookiescooker <fhhdhysd@gmail.com>
2024-05-24 22:29:24 +0700

    Update

---
+++ my-app/src/header/header_layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navi from '../navigation_bat/navigation_bar';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <Navi />
+    </div>
+  );
+}
+
+export default Header;
---
+++ my-app/src/header/navigation_bat/list_navigate.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navigation_bar";
+
+function Listnavigate () {
+  return (
+    <nav role="navigation" className="navi-list">
+      <ul>
+        <li>ACADEMY</li>
+        <li>RESTAURANTS & BARS</li>
+        <li>RECIPES</li>
+        <li>MASTERCLASSES</li>
+        <li>ABOUT GORDON</li>
+        <li>TY</li>
+        <li>CAREERS</li>
+        <li>GIFT</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Listnavigate;
